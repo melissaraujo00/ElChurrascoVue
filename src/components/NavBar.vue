b6debaa8b5f46fa34fe826a6c98479e92ce6fc5a
melissaraujo00 <daliarivas11@gmail.com>
2025-04-21 15:17:09 -0600

    barra de navegacion

---
+++ src/components/NavBar.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref } from "vue";
+
+const paginas = ref([
+  { name: 'Inicio', enlaceHTML: '#' },
+  { name: 'Menú', enlaceHTML: '#' },
+  { name: 'Reservar', enlaceHTML: '#' },
+  { name: 'Eventos', enlaceHTML: '#' },
+])
+</script>
+
+<template>
+  <nav class="bg-black text-white p-4">
+    <ul class="flex justify-between items-center">
+      <li class="text-red-500 font-bold text-lg">
+        <span class="text-red-500">EL CHURRASCO</span> <span class="text-white">RESTAURANTE</span>
+      </li>
+
+      <li>
+        <ul class="flex space-x-4">
+          <li
+            v-for="pagina in paginas"
+            :key="pagina.name"
+            class="hover:bg-red-500 p-2"
+          >
+            <a
+              :href="pagina.enlaceHTML"
+              class=" transition-colors duration-200"
+            >
+              {{ pagina.name }}
+            </a>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </nav>
+</template>

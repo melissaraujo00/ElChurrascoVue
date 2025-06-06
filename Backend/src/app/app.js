a6b5222b53eeea73a86423c88727daa4140ed34e
melissaraujo00 <daliarivas11@gmail.com>
2025-06-05 23:27:25 -0600

    api de login

--- Backend/src/app/app.js
+++ Backend/src/app/app.js
@@ -1,18 +1,20 @@
-//servidor
+// src/app/app.js
 import express from 'express';
 import morgan from 'morgan';
-import connectDB from '../database/database.js'; // ajusta la ruta si es necesario
+import connectDB from '../database/database.js'; 
+import cookieParser from 'cookie-parser';
+import login from '../router/login.js'; 
 
 const app = express();
-
-// Conexión a MongoDB
 connectDB();
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(morgan('dev'));
 
+app.use('/login', login); 
 app.get('/', (req, res) => {
-  res.send('This is express');
+  res.send('Bienvenido al Banckend del El Churrasco');
 });
 
 export default app;

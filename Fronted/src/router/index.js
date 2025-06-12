4bfb166cc3ab21e63fbe748050e4c42a3dc9cb78
Merlyn8989 <merlynrajo@gmail.com>
2025-06-12 03:19:24 -0600

    resolver conflicto

--- Fronted/src/router/index.js
+++ Fronted/src/router/index.js
@@ -7,6 +7,7 @@ import Register from '../components/Register.vue';
 import Login from '../components/Login.vue';
 import Cart from '../components/Cart.vue';
 import Users from '@/components/AdminComponets/Users.vue'
+import CrudSpecialities from "@/components/AdminComponets/Specialties/CrudSpecialities.vue"; 
 
 
 const routes = [
@@ -17,7 +18,10 @@ const routes = [
     { path: '/register', component: Register, name: 'Register' },
     { path: '/login', component: Login, name: 'Login' },
     { path: '/cart', component: Cart ,name: 'Cart'},
-    { path: '/user', component: Users, name: 'User'}
+    { path: '/user', component: Users, name: 'User'},
+    { path: '/cart', name: 'Cart'},
+    { path: '/cart', component:Cart ,name: 'Cart'},
+    { path: '/CrudSpecialities', component: CrudSpecialities, name: 'CrudSpecialities'}
 ]
 
 const router = createRouter({

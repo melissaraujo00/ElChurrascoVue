4fcfb7ce996bcdf4ef3e97f740d5f0ee15c5de9b
melissaraujo00 <daliarivas11@gmail.com>
2025-06-14 03:36:30 -0600

    version final

--- Fronted/src/components/HomeComponents/AboutUs.vue
+++ Fronted/src/components/HomeComponents/AboutUs.vue
@@ -13,8 +13,8 @@ const images = ref([
 
   <section class="flex flex-col md:flex-row gap-4 p-4 bg-neutral-950">
     <!-- Galería de imágenes a la izquierda, en grid 2x2 en desktop -->
-    <div class="p-3 m-2 w-full md:w-[55%] lg:w-[45%] shadow-md shadow-white transition-transform duration-300 hover:scale-105">
-      <div class="grid grid-cols-2 grid-rows-2 gap-4 h-full">
+    <div class="p-3 m-2 w-full md:w-[55%] lg:w-[45%] shadow-md transition-transform duration-300 hover:scale-105">
+      <div class="grid grid-cols-2 grid-rows-2 gap-4 h-full shadow-md  shadow-white">
         <img
           v-for="(img, index) in images"
           :key="index"
@@ -35,13 +35,13 @@ const images = ref([
       <div class="flex flex-col sm:flex-row justify-center items-center gap-4 p-4 w-full">
         <router-link
           :to="{ name: 'Reserve' }"
-          class="bg-red-700 text-white px-4 py-2 rounded hover:bg-red-800 transition-colors duration-200 flex items-center justify-center shadow-md w-full sm:w-auto"
+          class="bg-red-700 text-white px-4 py-2 rounded hover:bg-red-800 transition-colors duration-200 flex items-center justify-center shadow-md shadow-amber-50 w-full sm:w-auto"
         >
           Reservar Evento
         </router-link>
         <router-link
           :to="{ name: 'Menu' }"
-          class="bg-white text-black px-4 py-2 rounded hover:bg-gray-200 transition-colors duration-200 flex items-center justify-center shadow-md w-full sm:w-auto"
+          class="bg-white text-black px-4 py-2 rounded hover:bg-gray-200 transition-colors duration-200 flex items-center justify-center shadow-md shadow-red-500 w-full sm:w-auto"
         >
           Hacer Pedido
         </router-link>

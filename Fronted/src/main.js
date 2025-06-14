4939d206dd176cd8687b56a1b8086776b1698fda
Merlyn8989 <merlynrajo@gmail.com>
2025-06-14 02:25:53 -0600

    dialogo de confimación

--- Fronted/src/main.js
+++ Fronted/src/main.js
@@ -6,10 +6,13 @@ import App from './App.vue'
 import router from './router'
 import Toast from 'vue-toastification';
 import 'vue-toastification/dist/index.css';
+import VueSweetalert2 from 'vue-sweetalert2'
+import 'sweetalert2/dist/sweetalert2.min.css'
 
 const app = createApp(App)
 
 app.use(Toast); 
+app.use(VueSweetalert2)
 app.use(createPinia())
 app.use(router)
 app.mount('#app')
